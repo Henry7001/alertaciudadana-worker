9964050eadeebac41fd2e125ffc228eee59bbbaa
Henry7001 <armandy2001@gmail.com>
2024-07-27 01:21:40 -0500

    Agrega configuración de Express y endpoint de inicio de servidor

--- start.js
+++ start.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
+const express = require('express');
 const { execSync } = require('child_process');
+const app = express();
+const port = process.env.PORT || 4000;
 
-const command = `graphile-worker -c ${process.env.DATABASE_URL}`;
-execSync(command, { stdio: 'inherit' });
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+  try {
+    // Ejecuta el worker
+    console.log(`Starting worker...`);
+    execSync(`graphile-worker -c ${process.env.DATABASE_URL}`, { stdio: 'inherit' });
+  } catch (error) {
+    console.error('Error while starting the worker:', error);
+  }
+});
